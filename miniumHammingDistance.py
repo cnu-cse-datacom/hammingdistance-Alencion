35292f5d92ef10cf506fc1789cf609b37cdd433b
이정준 <rhqnrdl625@gmail.com>
2019-04-18 18:06:16 +0900

    파일명 pratice -> partice 수정

--- miniumHammingDistance.py
+++ miniumHammingDistance.py
@@ -1,14 +1,14 @@
 import random
 import numpy as np
 import pandas as pd
-from hamming_practice import hamming
+from hamming_parctice import hamming
 
 df = pd.read_csv('sample.csv', names = ['word', 'bin'])
 
 min = hamming(df.iloc[1,1], df.iloc[2,1])
 count = 1;
-for i in range(0,100):
-	for j in range(0,100):
+for i in range(0,len(df)):
+	for j in range(0,len(df)):
 		if i < j:
 			hd = hamming(df.iloc[i,1],df.iloc[j,1])
 			print(count,"(",df.iloc[i,0],df.iloc[j,0],")hamming_distance: ",hd)
